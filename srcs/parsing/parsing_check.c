07bc581e0b6e55d1eaf6e9861b87deabc6a29b57
spookier <costinalexandru777@gmail.com>
2023-03-13 09:49:05 +0100

    fix wall surround

--- srcs/parsing/parsing_check.c
+++ srcs/parsing/parsing_check.c
@@ -59,6 +59,37 @@ static int check_rectangle(t_pvars *v)
     return 0;
 }
 
+static int check_walls_surround(t_pvars *v)
+{
+	int j;
+
+	j = 0;
+	while(v->map[0][j])
+	{
+		if(v->map[0][j] == '1')
+			j++;
+		else
+			return(1);
+	}
+	j = 0;
+	while(v->map[v->rows_map - 1][j])
+	{
+		if(v->map[v->rows_map - 1][j] == '1')
+			j++;
+		else
+			return(1);
+	}
+	j = 0;
+	while (j < v->rows_map)
+	{
+		if (v->map[j][0] == '1' && v->map[j][v->chars_map - 1] == '1')
+			j++;
+		else
+			return (1);
+	}
+	return(0);
+}
+
 int init_check_map(t_pall *all)
 {
 	if(all->vars.rows_map < 3 || all->vars.chars_map < 2)
@@ -69,6 +100,12 @@ int init_check_map(t_pall *all)
 
 	if(check_rectangle(&all->vars) == 1 || init_items(all) == 1)
 		return(1);
+	if(check_walls_surround(&all->vars) == 1)
+	{
+		ft_printf("Error\nMap not surrounded by walls\n");
+		return(1);
+
+	}
 
 	printf("map checked\n");
 	return(0);
